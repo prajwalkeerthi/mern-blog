030688afeeae7fd924e36a37eaf5c41110aca49f
prajwalkeerthi <prajwal.keerthi@rhibhus.com>
2024-02-01 14:59:39 +0530

    signup UI backend is completed

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,44 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate  } from "react-router-dom";
 
 export default function Signup() {
+
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage]= useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();  
+
+  const handlechange = (e) =>{
+    setFormData({...formData, [e.target.id]: e.target.value.trim()})
+  };
+  const handleSubmit = async(e)  =>{
+    e.preventDefault();
+    if(!formData.username || !formData.email || !formData.password){
+      return setErrorMessage('Please fill out all fields.');
+    }
+    try {
+      setLoading(true);      
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signup',{
+        method: 'POST',
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if(res.ok){
+        navigate('/sign-in');
+      }
+      if(data.success === false){
+        return setErrorMessage(data.errorMessage)        
+      }
+      setLoading(false);
+      
+    } catch (error) {
+      setErrorMessage(error.errorMessage)  
+      setLoading(false); 
+     }
+  };
   return<div className="min-h-screen mt-20">
         <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
             <div className="flex-1">
@@ -16,29 +53,43 @@ export default function Signup() {
             </div>
             <div className="flex-1">
               {/*Right side */}
-                <form className="flex flex-col gap-4">
+                <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
                   <div>
                   <Label value='Your Username'/>
-                  <TextInput type="text" placeholder="Username" id='username'/>
+                  <TextInput type="text" placeholder="Username" id='username' onChange={handlechange}/>
                   </div>
                   <div>
                   <Label value='Your Email'/>
-                  <TextInput type="text" placeholder="email" id='email'/>
+                  <TextInput type="email" placeholder="email" id='email'onChange={handlechange}/>
                   </div>
                   <div>
                   <Label value='Your Password'/>
-                  <TextInput type="text" placeholder="Password" id='password'/>
+                  <TextInput type="password" placeholder="Password" id='password'onChange={handlechange}/>
                   </div>
-                  <Button gradientDuoTone='purpleToPink' type="submit">
-                    Sign Up
+                  <Button gradientDuoTone='purpleToPink' type="submit" disable={loading}>
+                  {
+                      loading ? (
+                        <>
+                        <Spinner size="sm"/>
+                        <span className="pl-3">Loading...</span>
+                        </>                        
+                      ):'Sign Up'
+                    }
                   </Button>                  
                 </form>
                 <div className="flex gap-2 text-sm mt-5">
                   <span>Have an account?</span>
-                  <Link to='/sign-in' className="text-blue-500">
+                  <Link to='/sign-in' className="text-blue-500" >
                     Sign In
                   </Link>
                 </div>
+                {
+                  errorMessage && (
+                    <Alert className="mt-5" color="failure">
+                      {errorMessage}
+                    </Alert>
+                  )
+                }
               </div>      
         </div>
     </div>
